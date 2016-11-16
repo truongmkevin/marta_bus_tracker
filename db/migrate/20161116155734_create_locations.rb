a3456878b4c20c344d8ec995da48f02610198056
KT <truongmkevin@yahoo.com>
2016-11-16 15:59:38 +0000

    scaffold for Location and migrated

---
+++ db/migrate/20161116155734_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locations do |t|
+      t.float :latitude
+      t.float :longitude
+      t.string :address
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end

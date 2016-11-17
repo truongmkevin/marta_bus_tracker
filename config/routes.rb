a4fb591902c8b70b4b69a3736f6b459207dacaa1
KT <truongmkevin@yahoo.com>
2016-11-17 14:41:10 +0000

    changed home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'locations#new'
 end
